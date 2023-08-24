92929d3776fa0f27f620dc894393dec4a2a5981c
Calvineries <Calvineries@gmail.com>
2023-08-25 01:11:19 +0200

    Update triggerbot.py

--- triggerbot.py
+++ triggerbot.py
@@ -1,3 +1,5 @@
+__author__ = 'github.com/Calvineries'
+
 import pymem
 import time
 import pyautogui
@@ -6,14 +8,16 @@ import pyautogui
 pm = pymem.Pymem('hl2.exe')
 client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
 engine = pymem.pymem.process.module_from_name(pm.process_handle, 'engine.dll').lpBaseOfDll
-OffLocalPlayer = pm.read_uint(client + 0x007377A4)
+
+LocalPlayer = pm.read_uint(client + 0x007377A4)
+LocalPlayerTarget = 0x3068
+MaxOnlinePlayers = 0x4F4000
 
 #TriggerBot
 while 0 != 1:
     time.sleep(0.01)
-    #If target player ID is less than online player counts
-    if pm.read_uint(OffLocalPlayer + 0x3068) <= pm.read_uint(engine + 0x4F4000):
+    #If target player ID is less than max online player
+    if pm.read_uint(LocalPlayer + LocalPlayerTarget) <= pm.read_uint(engine + MaxOnlinePlayers):
         #If target player ID is not 0
-        if pm.read_uint(OffLocalPlayer + 0x3068) != 0:
+        if pm.read_uint(LocalPlayer + LocalPlayerTarget) != 0:
             pyautogui.leftClick()
-
