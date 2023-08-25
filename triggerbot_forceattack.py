623feaf061e2aa2d1756e07826c1a0d23df7f932
Calvineries <Calvineries@gmail.com>
2023-08-25 08:59:59 +0200

    Add files via upload

---
+++ triggerbot_forceattack.py
@@ -0,0 +1,27 @@
+__author__ = 'github.com/Calvineries'
+
+import pymem
+import time
+
+#Memory
+pm = pymem.Pymem('hl2.exe')
+client = pymem.pymem.process.module_from_name(pm.process_handle, 'client.dll').lpBaseOfDll
+engine = pymem.pymem.process.module_from_name(pm.process_handle, 'engine.dll').lpBaseOfDll
+
+LocalPlayer = pm.read_uint(client + 0x007377A4)
+LocalPlayerTarget = 0x3068
+MaxOnlinePlayers = pm.read_uint(engine + 0x4F4000)
+ForceAttack = 0x7BBB70
+
+
+#TriggerBot
+while 0 != 1:
+    time.sleep(0.01)
+    TargetID = pm.read_uint(LocalPlayer + LocalPlayerTarget)
+    #If target player ID is less than max online player
+    if TargetID <= MaxOnlinePlayers:
+        #If target player ID is not 0
+        if TargetID != 0:
+            pm.write_uint(client + ForceAttack, 5)
+        else:
+            pm.write_uint(client + ForceAttack, 4)
